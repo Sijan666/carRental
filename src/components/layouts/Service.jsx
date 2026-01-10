149fa191d80b84b85bfc38cb27585cd7ba5f59f4
Sijan666 <666majharulislam@gmail.com>
2026-01-10 13:52:14 +0600

    service part started

---
+++ src/components/layouts/Service.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Container from '../Container'
+import Flex from '../Flex'
+
+const Service = () => {
+    return (
+        <>
+        <div className="py-50 bg-[url('/src/assets/serviceBg.png')]  bg-no-repeat bg-cover">
+            <Container>
+                <Flex>
+                    <div className="w-[58%]">
+                    </div>
+                    <div className="">
+                        <h3 className='text-[#181818] text-[48px] font-semibold w-152.5'>Explore Our Wide Range of Car Rental Services</h3>
+                    </div>
+                </Flex>
+            </Container>
+        </div>
+        </>
+    )
+}
+
+export default Service
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Banner from '../layouts/Banner'
 import Searchbar from '../layouts/Searchbar'
 import About from '../layouts/About'
+import Service from '../layouts/Service'
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Banner/>
         <Searchbar/>
         <About/>
+        <Service/>
         </>
     )
 }
