d68eb8811a60acb250d122ccb590d2a4ab5a281f
Sijan666 <666majharulislam@gmail.com>
2026-05-31 01:23:11 +0600

    add react lenis

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,26 @@ import Services from './components/pages/Services'
 import Fleets from './components/pages/Fleets'
 import Blogs from './components/pages/Blogs'
 import Error from './components/pages/Error'
+import ReactLenis from 'lenis/react'
 
 
 
 function App () {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<RootLayouts />}>
-          <Route index element={<Home/>} />
-          <Route path="/contact" element={<Contact/>} />
-          <Route path="/about" element={<Abouts/>} />
-          <Route path="/service" element={<Services/>} />
-          <Route path="/fleet" element={<Fleets/>} />
-          <Route path="/blog" element={<Blogs/>} />
-        </Route>
-        <Route path="*" element={<Error/>} />
-      </Routes>
+      <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
+        <Routes>
+          <Route path="/" element={<RootLayouts />}>
+            <Route index element={<Home/>} />
+            <Route path="/contact" element={<Contact/>} />
+            <Route path="/about" element={<Abouts/>} />
+            <Route path="/service" element={<Services/>} />
+            <Route path="/fleet" element={<Fleets/>} />
+            <Route path="/blog" element={<Blogs/>} />
+          </Route>
+          <Route path="*" element={<Error/>} />
+        </Routes>
+      </ReactLenis>
     </>
   )
 }
