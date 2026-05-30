b7c538c7bc548e8975f9e45e531597c5a424766d
Sijan666 <666majharulislam@gmail.com>
2026-05-31 01:38:30 +0600

    add react helmet for seo

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 import Banner from '../layouts/Banner'
 import Searchbar from '../layouts/Searchbar'
 import About from '../layouts/About'
@@ -11,6 +12,10 @@ import Blog from '../layouts/Blog'
 const Home = () => {
     return (
         <>
+        <Helmet>
+            <title>Home | Car Rental</title>
+            <meta name="description" content="Welcome to our website. Discover our fleets, services, and more." />
+        </Helmet>
         <Banner/>
         <Searchbar/>
         <About/>
