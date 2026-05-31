c10f9d590ba135505b664e791dba7037627f47ae
Sijan666 <666majharulislam@gmail.com>
2026-05-31 23:55:39 +0600

    add react hemlet for seo

--- src/components/pages/Blogs.jsx
+++ src/components/pages/Blogs.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Helmet } from 'react-helmet';
 
 export default function Blogs() {
     return (
         <div className="bg-amber-400 mt-35">
+            {/* React Helmet Meta Tags */}
+            <Helmet>
+                <title>Our Blog | My Website</title>
+                <meta 
+                    name="description" 
+                    content="Read our latest blog posts, news, and articles on various interesting topics." 
+                />
+                <meta name="keywords" content="Blog, Articles, News, Updates, Posts" />
+            </Helmet>
+            {/* Page Content */}
             <h1 className='text-center'>Blogs Page</h1>
         </div>
-    )
+    );
 }
