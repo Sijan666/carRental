f61204f08adf912d8bfc200abc2c86e11b139886
Sijan666 <666majharulislam@gmail.com>
2026-05-31 23:56:38 +0600

    add react hemlet for seo

--- src/components/pages/Fleets.jsx
+++ src/components/pages/Fleets.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Helmet } from 'react-helmet';
 
 export default function Fleet() {
     return (
         <div className="bg-amber-400 mt-35">
+            {/* React Helmet Meta Tags */}
+            <Helmet>
+                <title>Our Fleet | My Website</title>
+                <meta 
+                    name="description" 
+                    content="Explore our diverse fleet of vehicles. Find the perfect ride for your transportation needs." 
+                />
+                <meta name="keywords" content="Fleet, Vehicles, Cars, Transport, Services" />
+            </Helmet>
+            {/* Page Content */}
             <h1 className='text-center'>Fleet Page</h1>
         </div>
-    )
+    );
 }
