135ada76ac8551a6b14ba14774b5318b3223df68
Sijan666 <666majharulislam@gmail.com>
2026-05-31 23:52:53 +0600

    add react hemlet for seo

--- src/components/pages/Abouts.jsx
+++ src/components/pages/Abouts.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Helmet } from 'react-helmet';
 
 export default function Abouts() {
     return (
         <div className="bg-amber-400 mt-35">
+            {/* React Helmet Meta Tags */}
+            <Helmet>
+                <title>About Us | My Website</title>
+                <meta 
+                    name="description" 
+                    content="Learn more about our company, mission, and our amazing team on the About page." 
+                />
+                <meta name="keywords" content="About, Company, Team, Contact" />
+            </Helmet>
+            {/* Page Content */}
             <h1 className='text-center'>About Page</h1>
         </div>
-    )
+    );
 }
