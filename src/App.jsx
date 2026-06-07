4c01d805471ea5c243daf5d5606f9651282187f2
Sijan666 <666majharulislam@gmail.com>
2026-06-07 23:39:28 +0600

    add lazy loading with loader in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
+import React, { Suspense, lazy } from 'react'
 import './App.css'
-import RootLayouts from './components/layouts/RootLayouts'
-import Abouts from './components/pages/Abouts'
-import Contact from './components/pages/Contact'
-import Home from './components/pages/Home'
 import { Routes , Route } from 'react-router-dom'
-import Services from './components/pages/Services'
-import Fleets from './components/pages/Fleets'
-import Blogs from './components/pages/Blogs'
-import Error from './components/pages/Error'
 import ReactLenis from 'lenis/react'
+import Loader from './components/Loader'
 
-
+const RootLayouts = lazy(() => import('./components/layouts/RootLayouts'))
+const Home = lazy(() => import('./components/pages/Home'))
+const Contact = lazy(() => import('./components/pages/Contact'))
+const Abouts = lazy(() => import('./components/pages/Abouts'))
+const Services = lazy(() => import('./components/pages/Services'))
+const Fleets = lazy(() => import('./components/pages/Fleets'))
+const Blogs = lazy(() => import('./components/pages/Blogs'))
+const Error = lazy(() => import('./components/pages/Error'))
 
 function App () {
   return (
     <>
       <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothWheel: true }}>
-        <Routes>
-          <Route path="/" element={<RootLayouts />}>
-            <Route index element={<Home/>} />
-            <Route path="/contact" element={<Contact/>} />
-            <Route path="/about" element={<Abouts/>} />
-            <Route path="/service" element={<Services/>} />
-            <Route path="/fleet" element={<Fleets/>} />
-            <Route path="/blog" element={<Blogs/>} />
-          </Route>
-          <Route path="*" element={<Error/>} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<RootLayouts />}>
+              <Route index element={<Home/>} />
+              <Route path="/contact" element={<Contact/>} />
+              <Route path="/about" element={<Abouts/>} />
+              <Route path="/service" element={<Services/>} />
+              <Route path="/fleet" element={<Fleets/>} />
+              <Route path="/blog" element={<Blogs/>} />
+            </Route>
+            <Route path="*" element={<Error/>} />
+          </Routes>
+        </Suspense>
       </ReactLenis>
     </>
   )
